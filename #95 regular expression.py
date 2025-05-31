88baf2403534221cc40566436759c8d1795618c1
PYpranjul <pranjul.sharma15@gmail.com>
2025-06-01 02:22:47 +0530

    Delete #95 regular expression.py

--- #95 regular expression.py
+++
@@ -1,8 +0,0 @@
-import re
-pattern = r"\ba\b"
-# pattern2 =
-text = "The crimson platypus danced gracefully" \
-" beneath a sky filled with marshmallow clouds, while a confused squirrel composed jazz on a tiny, golden saxophone."
-match = re.finditer(pattern,text)
-for i in match:
-    print(i.group(),i.span())
